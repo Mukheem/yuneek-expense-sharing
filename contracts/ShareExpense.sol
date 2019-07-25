85ffeb6e78ffb7ffec66d6a95627c600323b7413
Mukheem <mukheemuddin@gmail.com>
2019-07-25 22:36:52 +0530

    ERC20 Implementation

--- contracts/ShareExpense.sol
+++ contracts/ShareExpense.sol
@@ -1,5 +1,23 @@
 pragma solidity ^0.5.9;
+import "./ERC20.sol";
 
-contract ShareExpense{
-    mapping (address => uint256) public balanceOf;
+contract ShareExpense is ShakiCoin{
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = 'SHAC V1.0';
+    uint256 public unitsOneEthCanBuy;
+    uint256 public totalEthInWei;
+    address public fundsWallet;
+
+    constructor() public {
+        balances[msg.sender] = 1000000000000000000000;
+        totalSupply = 1000000000000000000000;
+        name = "ShakiCoin";
+        decimals = 18;
+        symbol = "SHAC";
+        unitsOneEthCanBuy = 1;
+        fundsWallet = msg.sender;
+    }
+uint256 bal = balanceOf(0x6D29fb6A9F9682980dD68F95D0De075519842559);
 }
