8f096dad6cceb5e9528086f6c49f559dde7bd1c3
Mukheem <mukheemuddin@gmail.com>
2019-08-08 23:36:57 +0530

    Creation of Squads done

--- contracts/squads.sol
+++ contracts/squads.sol
@@ -5,51 +5,52 @@ contract Squads{
     struct squadMemberInfo{
         string memberName;
         address memberAddress;
-        //string _squadName;
+        //bool addressExists;
     }
     struct squadInfo{
+        bool isTaken;
         string squadName;
-        //address[] squadMembers;
+        address createdBy;
         squadMemberInfo[] squadMembers;
     }
-    squadInfo[] public SI;
-    //squadMemberInfo[] public SMI;
-    
 
     //Mapping to fetch SquadDetails when SquadName is provded
-    mapping(string => squadInfo) internal _squadDetails;
+    mapping(string => squadInfo) public squadDetails;
 
     //Mapping to track SquadName
     mapping(string => bool) public _squadNameExistence;
     
     //Function to create a Squad/Group and name that group
-    function createSquad(string memory _squadName)  public ifSquadnameAvailable(_squadName) returns(string memory isCreated){
-        _squadNameExistence[_squadName] = true;
-       /* SI.squadName = _squadName;
-        SI.squadMembers.push(_addressesToBeGrouped);
-        _squadNameExistence[_squadName] = true;*/
-        return _squadName;
+    function createSquad(string memory _squadName,string memory _memberName)  public ifSquadnameAvailable(_squadName){
+        squadDetails[_squadName].isTaken = true;
+        squadDetails[_squadName].squadName = _squadName;
+        squadDetails[_squadName].createdBy = msg.sender;
+        addMember(msg.sender,_memberName,_squadName);
     }
+    //Modifier to check if SquadName is already in use
     modifier ifSquadnameAvailable(string memory _squadName){
         require(
-            _squadNameExistence[_squadName] != true,"Oops..!!! Squad name already taken..."
+            squadDetails[_squadName].isTaken != true,"Oops..!!! Squad name already taken..."
         );
         _;
     }
-    //Function to add Members to an existing Squad
-    function addMember(address _memberAddress, string memory _memberName) public pure returns(uint8 isAdded){
-        squadMemberInfo[] memory squadMember = new squadMemberInfo[](1);
-        squadMemberInfo memory squadMemb = squadMemberInfo(_memberName,_memberAddress);
-        squadMember[0] = squadMemb;
-        //squadInfo memory squadInformation = squadInfo(_squadName,SMI);
-        //SI.push(squadInformation);
-        return 1;
+
+    //Modifier to check if SquadMember(Address) already exists in the squad [Modifier Excluded]
+    modifier isSquadMemeberAddressExisits(address  _squadMemberAddress,string memory _memberName,string memory _squadName){
+        squadMemberInfo[] memory squadMembers = squadDetails[_squadName].squadMembers;
+        for (uint i = 0; i <= squadMembers.length; i++){
+          require(squadMembers[i].memberAddress != _squadMemberAddress,"Address already exists in the Squad...!!!");
+          //require(keccak256(abi.encodePacked((squadMembers[i].memberName))) != keccak256(abi.encodePacked((_memberName))),
+          //"Member Name already exists in the Squad...!!!");
+         _;
+        }
     }
-
-    //Function to check if SquadName is already in use
-    /*function squadNameExists(string memory _squadName) public view returns(bool isExists){
-       
-        return(true);
-    }*/
-
+    //Function to add Members to an existing Squad
+    function addMember(address _memberAddress, string memory _memberName,string memory _squadName) public{
+        //squadMemberInfo[] memory squadMember = new squadMemberInfo[](1);
+        squadMemberInfo memory squadMember;
+        squadMember.memberName = _memberName;
+        squadMember.memberAddress = _memberAddress;
+        squadDetails[_squadName].squadMembers.push(squadMember);
+    }
 }
